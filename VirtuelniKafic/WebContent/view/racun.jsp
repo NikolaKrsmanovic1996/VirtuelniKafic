9afef912faa2207ab94890b522a44cfda812b121
Nikola <Nikola@192.168.0.15>
2019-07-24 22:44:59 +0200

    gotov projekat

--- VirtuelniKafic/WebContent/view/racun.jsp
+++ VirtuelniKafic/WebContent/view/racun.jsp
@@ -1,5 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@page import="model.User" %>
+<%@page import="java.util.ArrayList"%> 
+<%@page import="java.util.List" %>
+<%@page import="model.Artikal" %>  
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +11,43 @@
 <title>racun</title>
 </head>
 <body>
-
+      <%  User user = (User)session.getAttribute("ovdeJeUserObjekat");
+          double totalPrice = (Double)request.getAttribute("totalPrice");
+          List<Artikal> listaArtikala = (List<Artikal>)request.getAttribute("listaArtikala");
+          List<String> listaKolicina = (List<String>)request.getAttribute("listaKolicina");
+      
+      
+      
+      %>
+  User: <%=user.getUserName() %><br><br>
+  Racun: <table border="1">
+  
+               <tr>
+                <th>ime artikla</th>
+                <th>cena</th>
+                <th>popust</th>
+                <th>kolicina</th>
+                
+                </tr>
+                <% for (int i=0;i<listaArtikala.size();i++){ %>
+  
+                <tr>
+               <td><%= listaArtikala.get(i).getImeArtikla() %></td>
+               <td><%= listaArtikala.get(i).getCena() %></td>
+               <td><%= listaArtikala.get(i).getPopust() %></td>
+               <td><%= listaArtikala.get(i)%></td>
+               
+             
+              </tr>
+  
+          <% } %>
+    
+  </table>
+  <br><br>
+  
+  Total Price : <%= totalPrice  %>
+  
+  <a href="view/user.jsp">Back to User</a>
+  
 </body>
 </html>
--- VirtuelniKafic/src/Controller/PlatiRacunServlet.java
+++ VirtuelniKafic/src/Controller/PlatiRacunServlet.java
@@ -1,11 +1,25 @@
 package Controller;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+import dao.UserDao;
+import model.Artikal;
+import model.Racun;
+import model.User;
+import validacija.UserValidacija;
+
 
 /**
  * Servlet implementation class PlatiRacunServlet
@@ -19,6 +33,37 @@ public class PlatiRacunServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		UserValidacija uv = new UserValidacija();
+		UserDao ud = new UserDao();
+		
+		String [] check = request.getParameterValues("check");
+		String [] kolicina = request.getParameterValues("kolicina");
+		
+		
+		List<Artikal> listaArtikala = ud.artikliSaRacuna(check);
+		List<String> listaKolicina = uv.urediKolicinu(kolicina);
+		
+		
+		double totalPrice = ud.IznosRacuna(listaArtikala, listaKolicina);
+		
+		HttpSession session = request.getSession();
+		User user = (User)session.getAttribute("ovdeJeUserObjekat");
+		Date date = new Date();
+		Racun racun = ud.sacuvajRacun(user, date, listaArtikala);
+		
+		request.setAttribute("totalPrice", totalPrice);
+		request.setAttribute("listaArtikala", listaArtikala);
+		request.setAttribute("listaKolicina", listaKolicina);
+		
+		
+		if(user.getNovcanik()>=totalPrice) {
+			ud.updateNovcanik(user, totalPrice);
+			ud.updateStanje(listaArtikala, listaKolicina);
+			RequestDispatcher rd = request.getRequestDispatcher("view/racun.jsp");
+			rd.forward(request, response);
+		}else {
+			response.sendRedirect("view/user.jsp");
+		}
 		
 		
 		
